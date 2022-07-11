a8e21462f338d68ef3fa65dbe38a5136ff920f10
xnacly <matteogropp@protonmail.com>
2022-07-11 08:59:46 +0200

    feat: add color to fail test feedback

--- lib/libneoabacus/test/test.c
+++ lib/libneoabacus/test/test.c
@@ -28,11 +28,17 @@ int fail_t = 0;
 int success_t = 0;
 int cur_t = 0;
 
+/*
+ * compares two doubles
+ */
 int t_is_equal(double a, double b){
-    if((fabs(a)-fabs(b))<0.1) return 1;
+    if((fabs(a) - fabs(b)) < 0.1) return 1;
     else return 0;
 }
 
+/*
+ * implementation of assert method to log results and count fails and successes
+ */
 void t_assert(int condition, char* message){
     cur_t++;
     if(condition) { 
@@ -56,14 +62,18 @@ void t_assert(int condition, char* message){
 void t_equ(){
     double *roots = eq_roots(5, -3, 0, 0, 0);
     double *roots1 = eq_roots(2, -8, 6, 0, 0);
-    t_assert(t_is_equal(-0.6, roots[0]), "getting root of a linear equation");
-    t_assert(t_is_equal(3.0, roots1[1]) && t_is_equal(1.0, roots1[0]), "getting roots of a quadratic equation");
+    t_assert(t_is_equal(-0.6, roots[0]),
+            "getting root of a linear equation");
+    t_assert(t_is_equal(3.0, roots1[1]) 
+            && t_is_equal(1.0, roots1[0]), 
+            "getting roots of a quadratic equation");
     free(roots);
     free(roots1);
 }
 
 void t_basic(){
-    t_assert(t_is_equal(3, root(27, 3)), "calculate the 3d root of 12");
+    t_assert(t_is_equal(3, root(27, 3)), 
+            "calculate the 3d root of 12");
 }
 
 void t_matrix(){
@@ -113,9 +123,16 @@ void t_matrix(){
     for(int i = 0; i < 3; i++) free(values1[i]);
     free(values1);
 
-    t_assert(t_is_equal(m2->values[0][0], 4) && t_is_equal(m2->values[0][1], 4) && t_is_equal(m2->values[1][0], 4) && t_is_equal(m2->values[1][1], 4), "adding matrices test");
-    t_assert(t_is_equal(m3->values[0][0], 0) && t_is_equal(m3->values[0][1], 0) && t_is_equal(m3->values[1][0], 0) && t_is_equal(m3->values[1][1], 0), "subtracting matrices test");
-    t_assert(t_is_equal(t, 4) && t_is_equal(t1, 6), "trace matrix test");
+    t_assert(t_is_equal(m2->values[0][0], 4) &&
+            t_is_equal(m2->values[0][1], 4) && 
+            t_is_equal(m2->values[1][0], 4) && 
+            t_is_equal(m2->values[1][1], 4), "adding matrices test");
+    t_assert(t_is_equal(m3->values[0][0], 0) && 
+            t_is_equal(m3->values[0][1], 0) && 
+            t_is_equal(m3->values[1][0], 0) && 
+            t_is_equal(m3->values[1][1], 0), "subtracting matrices test");
+    t_assert(t_is_equal(t, 4) 
+            && t_is_equal(t1, 6), "trace matrix test");
 
     mtrx_destroy(m);
     mtrx_destroy(m1);
@@ -134,9 +151,9 @@ void end(clock_t begin){
     double t = (double)(end-begin) / CLOCKS_PER_SEC;
 
     printf("test result: %s%s%s; total: %d; %d (%d%%) passed; %d (%d%%) failed; finished in: %.2fs\n",
-            fail_t == 0 ? ANSI_COLOR_GREEN : "",
+            fail_t == 0 ? ANSI_COLOR_GREEN : ANSI_COLOR_RED,
             fail_t == 0 ? "ok" : "fail",
-            fail_t == 0 ? ANSI_RESET : "",
+            ANSI_RESET,
             total_t,
             success_t,
             success_perc,
